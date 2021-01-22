142294adc31bb7868901d63495c0d27e87ce1359
Sherman <gvera.wx@gmail.com>
2021-01-21 22:55:47 -0400

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,185 @@
+<template>
+  <q-layout view="lHh Lpr lFf" style="font-size: 8pt">
+    <q-header elevated>
+      <q-toolbar class="bg-brand">
+        <q-btn
+          flat
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-input
+          v-model="search"
+          rounded
+          standout="bg-white text-dark"
+          label="Buscar"
+          input-class="text-dark"
+          dense
+          bg-color="white"
+          class="q-ma-xs col-xs-4 text-dark"
+        >
+          <template v-slot:prepend>
+            <q-icon v-if="search === ''" name="search" class="cursor-pointer" color="black"/>
+            <q-icon v-else name="close" class="cursor-pointer" color="black"/>
+          </template>
+        </q-input>
+
+        <q-space />
+        <q-avatar>
+          <img src="~assets/charly-face.png">
+        </q-avatar>
+        <div style="display: flex;justify-content: center;align-items: center;margin-top: 0px; margin-left: 5px"><span style="display: inline-block; vertical-align: top;font-size: 9pt;" class="bg-brand">{{ user }}</span></div>
+      </q-toolbar>
+
+      <q-bar inset class="bg-grey-9" style="padding: 5px 2% 5px 2%;height: 40px">
+        <div class="row full-width" style="display: flex; justify-content: space-between">
+          <div class="row">
+            <q-btn size="10px" padding="0px 15px" outline color="grey-3" label="Crear nuevo encargo" icon-right="add" no-caps style="height: 30px;"/>
+            <q-btn size="10px" padding="0px 15px" outline color="grey-3" label="Seleccionar Cliente" icon-right="mdi-database" no-caps class="q-ml-md" style="height: 30px;" />
+            <div style="display: flex;justify-content: center;align-items: center;margin-top: 0px; margin-left: 40px"><span style="display: inline-block; vertical-align: top;font-size: 9pt">{{ title }}</span></div>
+          </div>
+          <div style="margin-top: 0px; flex-grow: 2">
+            <q-btn size="12px" padding="0px 15px" style="position: relative;float: right;height: 30px;" outline color="grey-3" label="Cerrar Sesion" no-caps class="q-ml-md"  />
+          </div>
+        </div>
+      </q-bar>
+    </q-header>
+    <q-footer elevated>
+      <q-bar class="bg-brand">
+        <img src="~assets/copyright.png">
+        <q-space/>
+        <q-icon name="ion-logo-facebook" size="xs" class="text-teal-5"/>
+        <q-icon name="ion-logo-pinterest" size="xs" class="text-teal-5"/>
+        <q-icon name="ion-logo-instagram" size="xs" class="text-teal-5"/>
+        <q-icon name="ion-logo-twitter" size="xs" class="text-teal-5"/>
+        <q-icon name="ion-logo-youtube" size="xs" class="text-teal-5"/>
+        <q-icon name="ion-logo-linkedin" size="xs" class="text-teal-5"/>
+      </q-bar>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-brand"
+      :width="250"
+    >
+      <q-list align="center" dense>
+        <img class="q-ma-md" src="~assets/example1.png"/>
+        <q-item
+          clickable
+          v-for="(menu, index) of essentialLinks"
+          :key="index"
+        >
+          <q-item-section avatar top>
+            <q-avatar class="text-dark" size="md">
+              <q-icon v-if="!menu.noAssets" size="md">
+                <img :src="menu.icon" />
+              </q-icon>
+              <q-icon v-else color="grey-4" size="sm" :name="menu.icon"/>
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-left text-grey-4">{{ menu.title }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+const linksData = [
+  {
+    title: 'Home',
+    icon: require('../assets/example2.png')
+  },
+  {
+    title: 'VIIA Diseño',
+    icon: require('../assets/example3.png')
+  },
+  {
+    title: 'VIIA Movilida',
+    icon: require('../assets/example4.png')
+  },
+  {
+    title: 'VIIA Nativo',
+    icon: require('../assets/example5.png')
+  },
+  {
+    title: 'VIIA Moduliar',
+    icon: require('../assets/example6.png')
+  },
+  {
+    title: 'VIIA Sur',
+    icon: require('../assets/example7.png')
+  },
+  {
+    title: 'VIIA Inventor',
+    icon: require('../assets/example8.png')
+  },
+  {
+    title: 'Perfil de Usuario',
+    icon: 'folder',
+    noAssets: true
+  },
+  {
+    title: 'Contratos',
+    icon: 'close',
+    noAssets: true
+  },
+  {
+    title: 'Estados de Pago',
+    icon: 'close',
+    noAssets: true
+  },
+  {
+    title: 'Encargo',
+    icon: 'add',
+    noAssets: true
+  },
+  {
+    title: 'Planificacion & Requerimientos',
+    icon: 'mdi-database',
+    noAssets: true
+  },
+  {
+    title: 'Networking',
+    icon: 'emoji_objects',
+    noAssets: true
+  },
+  {
+    title: 'Cerrar Sesion',
+    icon: 'close',
+    noAssets: true
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      search: '',
+      title: 'Administrador Cuenta Cliente',
+      user: 'Benito Reyes',
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.bg-brand {
+  background: #363536;
+}
+.bg-second {
+  background: #363535;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,282 @@
+<template>
+  <q-page style="padding: 15px 20px 5px 20px; background: rgb(10,159,178);background: linear-gradient(180deg, rgba(10,159,178,1) 0%, rgba(8,174,195,1) 23%, rgba(0,229,255,1) 100%);">
+    <div style="width: 100%">
+      <q-card style="background-color: inherit" class="q-pt-none q-pb-xs" flat>
+        <q-card-section horizontal class="q-pb-none">
+          <q-item style="padding: 0px">
+            <q-item-section avatar class="justify-end q-pt-xs">
+              <q-btn class="q-mr-none q-ml-md" style="border: solid 2px;color: #888384" round outline dense size="25px" icon="person"/>
+            </q-item-section>
+          </q-item>
+          <q-card-section class="q-pt-md q-pl-none">
+            <div style="color: #344151">
+              <span>{{form.proyect}}</span>
+              <br/>
+              <span>{{form.id}}</span>
+              <br/>
+              <span>{{form.address}}</span>
+            </div>
+          </q-card-section>
+          <q-separator vertical/>
+          <q-card-section class="q-pt-md q-pl-md">
+            <div style="color: #344151">
+              <span>{{form.description}}</span>
+              <br/>
+              <span>Fecha Inicio: {{form.dateIn}}</span>
+              <br/>
+              <span>Fecha Termino: {{form.dateOut}}</span>
+            </div>
+          </q-card-section>
+          <q-separator vertical/>
+          <q-card-section class="q-pt-md q-pl-md">
+            <div style="color: #344151">
+              <span>Superficie: {{form.area}}</span>
+              <br/>
+              <span>Encargado Proyecto: {{form.boss}}</span>
+              <br/>
+              <span>Presupuesto Estimado: {{form.value}}</span>
+              <br/>
+              <span>{{form.company}}</span>
+            </div>
+          </q-card-section>
+          <q-card-section class="q-ml-xl justify-center">
+            <img class="col-1" style="height: 60px" src="~assets/VIIA SUR.png" />
+            <img class="col-1" style="height: 60px" src="~assets/v1.jpeg" />
+            <img class="col-1" style="height: 60px" src="~assets/VIIA SUR.png" />
+            <img class="col-1" style="height: 60px" src="~assets/v1.jpeg" />
+            <img class="col-1" style="height: 60px" src="~assets/VIIA SUR.png" />
+            <img class="col-1" style="height: 60px" src="~assets/v1.jpeg" />
+          </q-card-section>
+
+        </q-card-section >
+      </q-card>
+    </div>
+    <div class="row justify-center example-container full-width">
+      <div class="example-cel col-md-2" v-for="(button, index) in buttons" :key="index" tabindex="0">
+        <q-btn class="q-mx-xs" size="10px" :text-color="button.color" padding="0px 0px" no-caps style="background-color: #455264; height: 25px; max-width: 100%; min-width: 140px; width: 95%" :label="button.text" @click="changeColor(index)"/>
+      </div>
+    </div>
+    <hr style="border: 1px dashed #455264" class="q-mt-sm"/>
+    <div class="full-width">
+      <q-card>
+        <q-bar style="background-color: #455264;" class="justify-center"><span style="font-size: 10pt" class="text-grey-3">{{ panel.description }}</span></q-bar>
+        <q-card-section horizontal class="q-py-sm">
+          <div class="row justify-evenly full-width">
+            <div class="col q-pa-xs">
+              <q-bar style="background-color: #455264;" class="justify-center"><span style="font-size: 10pt" class="text-grey-3">{{ panel.team.title }}</span></q-bar>
+              <q-card>
+                <q-scroll-area style="height: 265px">
+                  <q-list bordered class="bg-light-blue-1">
+                    <q-item v-for="contact in contacts" :key="contact.id" class="q-my-md bg-white q-mx-sm" clickable v-ripple>
+                      <q-item-section avatar class="q-pt-xs">
+                        <q-btn class="q-ma-none" style="border: solid 2px;color: #888384" round outline dense size="md" icon="person"/>
+                      </q-item-section>
+
+                      <q-item-section>
+                        <q-item-label style="word-wrap: break-word;">{{ contact.name }}</q-item-label>
+                      </q-item-section>
+
+                      <q-item-section side>
+                        <div class="row items-center">
+                          <q-btn text-color="grey-3" style="background-color: #888384; height: 25px; widht: 30px; font-size: 8pt; display: flex; justify-content: center; align-items: center;" label="Ficha" no-caps/>
+                          <q-checkbox v-model="contact.val" />
+                        </div>
+                      </q-item-section>
+
+                    </q-item>
+                  </q-list>
+                </q-scroll-area>
+              </q-card>
+            </div>
+            <div class="col q-pa-xs">
+              <q-bar style="background-color: #455264;" class="justify-center"><span style="font-size: 10pt" class="text-grey-3">{{ panel.team.title }}</span></q-bar>
+              <q-card>
+                <q-scroll-area style="height: 265px">
+                  <q-list bordered class="bg-light-blue-1">
+                    <q-item v-for="contact in contacts" :key="contact.id" class="q-my-md bg-white q-mx-sm" clickable v-ripple>
+                      <q-item-section avatar class="q-pt-xs">
+                        <q-btn class="q-ma-none" style="border: solid 2px;color: #888384" round outline dense size="md" icon="person"/>
+                      </q-item-section>
+
+                      <q-item-section>
+                        <q-item-label style="word-wrap: break-word;">{{ contact.name }}</q-item-label>
+                      </q-item-section>
+
+                      <q-item-section side>
+                        <div class="row items-center">
+                          <q-btn text-color="grey-3" style="background-color: #888384; height: 25px; widht: 30px; font-size: 8pt; display: flex; justify-content: center; align-items: center;" label="Ficha" no-caps/>
+                          <q-checkbox v-model="contact.val" />
+                        </div>
+                      </q-item-section>
+
+                    </q-item>
+                  </q-list>
+                </q-scroll-area>
+              </q-card>
+            </div>
+            <div class="col q-pa-xs">
+              <q-bar style="background-color: #455264;" class="justify-center"><span style="font-size: 10pt" class="text-grey-3">{{ panel.team.title }}</span></q-bar>
+              <q-card>
+                <q-scroll-area style="height: 265px">
+                  <q-list bordered class="bg-light-blue-1">
+                    <q-item v-for="contact in contacts" :key="contact.id" class="q-my-md bg-white q-mx-sm" clickable v-ripple>
+                      <q-item-section avatar class="q-pt-xs">
+                        <q-btn class="q-ma-none" style="border: solid 2px;color: #888384" round outline dense size="md" icon="person"/>
+                      </q-item-section>
+
+                      <q-item-section>
+                        <q-item-label style="word-wrap: break-word;">{{ contact.name }}</q-item-label>
+                      </q-item-section>
+
+                      <q-item-section side>
+                        <div class="row items-center">
+                          <q-btn text-color="grey-3" style="background-color: #888384; height: 25px; widht: 30px; font-size: 8pt; display: flex; justify-content: center; align-items: center;" label="Ficha" no-caps/>
+                          <q-checkbox v-model="contact.val" />
+                        </div>
+                      </q-item-section>
+
+                    </q-item>
+                  </q-list>
+                </q-scroll-area>
+              </q-card>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+const contacts = [
+  {
+    id: 1,
+    name: 'Ruddy Jedrzej',
+    email: 'rjedrzej0@discuz.net',
+    val: false
+  },
+  {
+    id: 2,
+    name: 'Mallorie Alessandrini',
+    email: 'malessandrini1@marketwatch.com',
+    val: false
+  },
+  {
+    id: 3,
+    name: 'Elisabetta Wicklen',
+    email: 'ewicklen2@microsoft.com',
+    val: false
+  },
+  {
+    id: 4,
+    name: 'Seka Fawdrey',
+    email: 'sfawdrey3@wired.com',
+    val: false
+  }
+]
+
+const offline = [
+  {
+    id: 5,
+    name: 'Brunhilde Panswick',
+    email: 'bpanswick4@csmonitor.com',
+    avatar: 'avatar2.jpg'
+  }, {
+    id: 6,
+    name: 'Winfield Stapforth',
+    email: 'wstapforth5@pcworld.com',
+    avatar: 'avatar6.jpg'
+  }
+]
+
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      val: false,
+      contacts,
+      offline,
+      buttons: [
+        { text: 'Encargo', color: 'teal-5' },
+        { text: 'Admi. Networking', color: 'teal-5' },
+        { text: 'Productos Paypal', color: 'teal-5' },
+        { text: 'Planificacion', color: 'teal-5' },
+        { text: 'Notificaciones', color: 'teal-5' },
+        { text: 'Filtro Comunicaciones', color: 'teal-5' }
+      ],
+      form: {
+        proyect: 'Proyecto Casa Vera & Del Barrio',
+        id: '01-000-000',
+        address: 'Urb. Quincheumo',
+        description: 'Predio PL-03',
+        dateIn: '15/08/2020',
+        dateOut: '15/12/2020',
+        area: '500m2',
+        boss: 'Pablo Aristia',
+        value: 3000 + ' UF',
+        company: 'VIIA Diseño'
+      },
+      panel: {
+        description: 'Detalle Panel Experto',
+        customers: {
+          title: 'Clientes',
+          data: [
+            {
+              name: 'Experto A.',
+              id: 1,
+              val: false
+            },
+            {
+              name: 'Experto A.',
+              id: 2,
+              val: false
+            },
+            {
+              name: 'Experto A.',
+              id: 3,
+              val: false
+            },
+            {
+              name: 'Experto A.',
+              id: 4,
+              val: false
+            },
+            {
+              name: 'Experto A.',
+              id: 5,
+              val: false
+            },
+            {
+              name: 'Experto A.',
+              id: 6,
+              val: false
+            }
+          ]
+        },
+        team: {
+          title: 'Equipo'
+        },
+        advisers: {
+          title: 'Asesores'
+        }
+      }
+    }
+  },
+  methods: {
+    changeColor (id) {
+      return this.buttons.forEach((ele, index) => { if (index === id) ele.color = 'grey-3'; else ele.color = 'teal-5'; return ele })
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+.example-container
+
+  .example-cell
+    margin: 1px
+    padding: 2px 8px
+    box-shadow: inset 0 0 0 2px $grey-6
+  .bg-brand
+    background: #363536
+
+</style>
